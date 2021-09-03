570d28409abb709f8d95415aa28e6cce1f3c398a
pjpmsousa <88352014+pjpmsousa@users.noreply.github.com>
2021-09-03 18:16:46 +0100

    Update App.tsx
    remove link

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,6 @@ function App() {
         <p>
           New react app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
