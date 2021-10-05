a16cf8445f1386e017e8797cc47dbb04d715adaf
pjpmsousa <88352014+pjpmsousa@users.noreply.github.com>
2021-10-05 16:19:08 +0100

    added unit tests for badge component

--- src/components/urbint/badge/Badge.test.tsx
+++ src/components/urbint/badge/Badge.test.tsx
@@ -1,1 +1,27 @@
-export default {}
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { LevelEnum } from "../utils/LevelEnum";
+import Badge from "./Badge";
+
+describe("Badge", () => {
+  test("Check if correct risk level is displayed in BADGE", () => {
+    render(<Badge isBadge riskLevel={LevelEnum.medium} />);
+
+    const labelMedium = screen.getByText("MEDIUM");
+    expect(labelMedium).toBeInTheDocument();
+  });
+
+  test("Check if correct risk level is displayed in LABEL", () => {
+    render(<Badge isBadge={false} riskLevel={LevelEnum.veryHigh} />);
+
+    const labelVeryHigh = screen.getByText("VERY HIGH RISK");
+    expect(labelVeryHigh).toBeInTheDocument();
+  });
+
+  test("Check for correct background color in badge", () => {
+    render(<Badge isBadge riskLevel={LevelEnum.medium} />);
+
+    const badge = screen.getByText("MEDIUM");
+    expect(badge.classList.contains("badge-medium")).toBe(true);
+  });
+});
