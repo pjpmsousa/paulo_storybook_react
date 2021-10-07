fe793dfa82ecac23b4a9df3011104b177d2b6a98
pjpmsousa <88352014+pjpmsousa@users.noreply.github.com>
2021-10-07 18:28:43 +0100

    add + config Tailwind + update Navigation

--- src/components/urbint/navigation/Navigation.tsx
+++ src/components/urbint/navigation/Navigation.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./Navigation.module.css";
 import urbint from "./urbint.svg";
 import bell from "./bell.svg";
 
@@ -11,12 +10,12 @@ interface NavigationProps {
 const Navigation = ({isIconVisible}: NavigationProps) => {
   return (
     <>
-      <div className={styles.wrapper}>
-        <div className={styles.company}>
-          <img src={urbint} className={styles.logo} alt="logo" />
+      <div className="flex items-center p-4 bg-gray-700 font-sans font-bold text-white justify-between">
+        <div className="flex items-center">
+          <img src={urbint} className=" h-5 pr-3" alt="logo" />
           <div>DAMAGE PREVENTION</div>
         </div>
-        {isIconVisible && <img src={bell} className={styles.icon} alt="bell" />}
+        {isIconVisible && <img src={bell} className="h-4 hidden sm:block" alt="bell" />}
       </div>
     </>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens : {'sm': '300px'},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
